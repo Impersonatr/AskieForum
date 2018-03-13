15eec9e549513159b43976e89ff5b93264aa979b
hydra314 <multi.goals@gmail.com>
2018-03-13 14:57:25 -0700

    Set up for passing data between site and database

--- server.js
+++ server.js
@@ -20,102 +20,153 @@ var con = mysql.createConnection({
   port: 3001
 });
 
-con.connect(function(err){
-  if(err) throw err;
+con.connect();
 
-  app.get('/', function (req, res) 
-  {
-    console.log("== Got request for", req.url);
-    res.render('index-page', 
-    {
-      title: "Welcome"
-      // userData: usersData
-  	});
+/*function getUserData(callback){
+  con.query("SELECT * FROM user", function(err, result){
+    if(err) callback(err, null);
+    else callback(null, result);
   });
+}
 
-  app.get('/login', function (req, res)
-  {
-    console.log("== Got request for", req.url);
-    res.render('login-page',
-    {
-      title: "Login"
-    });
-      
-    
+
+return con.query("SELECT * FROM user", function selectAll(err, result, fields){
+    if(err) throw err;
+    for(var i in result)
+      console.log(result[i]);
   });
+}*/
 
+con.query("SELECT * FROM forum_user", function(err, result, fields){
+  forumUserData = result;
+});
 
-  app.get('/home', function (req, res)
+con.query("SELECT * FROM forum_host", function(err, result, fields){
+  forumHostData = result;
+});
+
+con.query("SELECT * FROM forum", function(err, result, fields){
+  forumData = result;
+});
+
+con.query("SELECT * FROM forum_member", function(err, result, fields){
+  forumMemberData = result;
+});
+
+con.query("SELECT * FROM session", function(err, result, fields){
+  sessionData = result;
+});
+
+con.query("SELECT * FROM question", function(err, result, fields){
+  questionData = result;
+});
+
+con.query("SELECT * FROM response", function(err, result, fields){
+  responseData = result;
+});
+
+app.get('/', function (req, res) 
+{
+  console.log("== Got request for", req.url);
+  res.render('index-page', 
   {
-    console.log("== Got request for", req.url);
-    res.render('home-page', 
-    {
-      title: "Forum List"
-    });
-  });
-
-  app.get('/settings', function (req, res)
-  {
-    console.log("== Got request for", req.url);
-    res.render('user-settings', 
-    {
-      title: "Settings"
-    });
-  });
-
-  app.get('/signup', function (req, res)
-  {
-    console.log("== Got request for", req.url);
-    res.render('signup-page', 
-    {
-      title: "Sign-Up"
-    });
-  });
-
-  app.get('/member-forum', function (req, res)
-  {
-    console.log("== Got request for", req.url);
-    res.render('member-forum-page', 
-    {
-      title: "Forum"
-    });
-  });
-
-  app.get('/host-forum', function (req, res)
-  {
-    console.log("== Got request for", req.url);
-    res.render('host-forum-page', 
-    {
-      title: "Forum"
-    });
-  });
-
-  app.post('/_newUserInsert', function (req, res)
-  {
-    console.log("== Got POST request for", req.url);
-    res.status(200).send("You tried to create a user successfully, (Press Back)");  
-
-  });
-
-  app.post('/_loginGetResult', function (req, res)
-  {
-    console.log("== Got POST request for", req.url);
-    res.status(200).send("tried to log in successfully, (Press Back)");  
-
-  });
-
-
-
-  app.get('*', function (req, res) 
-  {
-    console.log("== Got request for", req.url);
-    res.status(404).render('404-page',
-    {
-    	title: "Error 404"
-    });
-  });
-
-  app.listen(port, function () {
-    console.log("== Listening on port", port);
+    title: "Welcome"
   });
 });
+
+app.get('/login', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  con.query("SELECT * FROM user", function(err, result, fields){
+    userData = result;
+    res.render('login-page',
+    {
+      title: "Login",
+      userData: userData
+    });
+  });
+});
+
+
+app.get('/home', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  res.render('home-page', 
+  {
+    title: "Forum List"
+  });
+});
+
+app.get('/about', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  res.render('about',
+  {
+    title: "About"
+  });
+});
+
+app.get('/settings', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  res.render('user-settings', 
+  {
+    title: "Settings"
+  });
+});
+
+app.get('/signup', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  res.render('signup-page', 
+  {
+    title: "Sign-Up"
+  });
+});
+
+app.get('/member-forum', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  res.render('member-forum-page', 
+  {
+    title: "Forum"
+  });
+});
+
+app.get('/host-forum', function (req, res)
+{
+  console.log("== Got request for", req.url);
+  res.render('host-forum-page', 
+  {
+    title: "Forum"
+  });
+});
+
+app.post('/_newUserInsert', function (req, res)
+{
+  console.log("== Got POST request for", req.url);
+  res.status(200).send("You tried to create a user successfully, (Press Back)");  
+
+});
+
+app.post('/_loginGetResult', function (req, res)
+{
+  console.log("== Got POST request for", req.url);
+  res.status(200).send("tried to log in successfully, (Press Back)");  
+
+});
+
+app.get('*', function (req, res) 
+{
+  console.log("== Got request for", req.url);
+  res.status(404).render('404-page',
+  {
+  	title: "Error 404"
+  });
+});
+
+app.listen(port, function () {
+  console.log("== Listening on port", port);
+});
+
+con.end();
